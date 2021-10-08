15e37fb7deeebcdb74b3ed345b0a50d2258e9d9e
Charles <63302440+Sarrus1@users.noreply.github.com>
2021-10-09 01:09:08 +0200

    Add a parser to get the version of the .sp plugin (#4)
    * Add a parser for plugin versions
    
    * Added webpack to bundle the files
    
    * Fix https to http redirect error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core';
 import { installCompiler } from './installer';
+import * as path from "path";
+import * as fs from "fs";
+import { parseFile } from "./utils/parser";
 
 async function run() {
     try {
@@ -8,7 +11,17 @@ async function run() {
         let version = await installCompiler(range);
         core.setOutput('version', version);
 
-    } catch (error) {
+        const versionFile = core.getInput('version-file', { required: false });
+        const defineName = core.getInput('define-name', { required: false });
+        if(versionFile !== "") {
+            if(!fs.existsSync(versionFile)) {
+                core.error("The path of the file containing the version is incorrect.");
+                return;
+            }
+            let parsedVersion = parseFile(versionFile, defineName);
+            core.setOutput('plugin-version', parsedVersion);
+        }
+    } catch (error: any) {
         core.setFailed(error.message);
     }
 }
---
+++ src/utils/parser.ts
@@ -0,0 +1,21 @@
+﻿import * as fs from "fs";
+
+export function parseFile(file: string, defineName: string): string{
+  if(defineName !== ""){
+      var regex = new RegExp(`\\s*\\#define\\s+${defineName}\\s+(?:\\"|\\')([\\d.]+)`);
+  }
+  else{
+      var regex = /\s*version\s*=\s*(?:\"|\')([\d.]+)/;
+  }
+  const data = fs.readFileSync(file, 'utf8').split("\n");
+  var parsedVersion = "";
+  var match: RegExpMatchArray|null;
+  for(let line of data){
+      match = line.match(regex);
+      if(match !== null){
+          parsedVersion = match[1].trim();
+          break;
+      }
+  }
+  return parsedVersion;
+}
