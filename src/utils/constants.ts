af544c3453b5b3a8bdee6f25a3f57428f100ab3c
Sarrus1 <omegasix0611@gmail.com>
2021-10-08 22:46:23 +0200

    Fix https to http redirect error

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const ENDPOINT: string = 'https://www.sourcemod.net/smdrop';
+export const ENDPOINT: string = 'http://www.sourcemod.net/smdrop';
 export const MM_REGEX: RegExp = /href="(.*?)"/g;
 export const BUILD_REGEX: RegExp = /href="sourcemod-[0-9]+.[0-9]+.[0-9]+-git([0-9]+)-(linux|windows|mac).(.*?)"/g;
