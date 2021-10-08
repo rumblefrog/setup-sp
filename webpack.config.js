7972d6fc273f97796892d2f9dbe9a9fbffd1aeaf
Sarrus1 <omegasix0611@gmail.com>
2021-10-08 22:33:31 +0200

    Added webpack to bundle the files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+﻿const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: "production",
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+};
