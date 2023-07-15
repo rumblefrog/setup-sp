41acc1b81723a05df1068e0477be537ed24cd9c9
Flleeppyy <18307183+flleeppyy@users.noreply.github.com>
2023-07-15 10:11:55 -0700

    Update parser.ts to fully support semver

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,7 +2,7 @@
 
 export function parseFile(file: string, defineName: string): string{
   if(defineName !== ""){
-      var regex = new RegExp(`\\s*\\#define\\s+${defineName}\\s+(?:\\"|\\')([\\d.]+)`);
+      var regex = new RegExp(/\s*\#define\s+/.source + defineName + /\s+(?:"|\')((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)/.source);
   }
   else{
       var regex = /\s*version\s*=\s*(?:\"|\')([\d.]+)/;
